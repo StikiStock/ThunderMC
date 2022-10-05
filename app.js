65c13bfaa6c7c18dd5a0aaf40d73ba4e376dda8b
Stiki Stock <mamastiki7@gmail.com>
2022-10-05 21:20:58 +0330

    Huge optimizations

--- app.js
+++ app.js
@@ -14,11 +14,20 @@ const animated = document.querySelectorAll('.animated');
 animated.forEach((el) => observer.observe(el));
 
 function copy_text() {
-    var copyText = document.getElementById("ip-text");  
-    var input = document.createElement("textarea");
+    const copyText = document.querySelector('#ip');  
+    const input = document.createElement("textarea");
     input.value = copyText.textContent;
     document.body.appendChild(input);
     input.select();
     document.execCommand("Copy");
     input.remove();
+    const copybutton = document.querySelector('#copybutton')
+    copybutton.innerHTML = "Done"
+    copybutton.focus()
+    setTimeout(() => {
+        copybutton.innerHTML = "Copy"
+        copybutton.blur()
+    }, 1550);
+    
+    
 };

def674453bc3a003d9eb9ec56797c795676a99d5
Stiki Stock <mamastiki7@gmail.com>
2022-10-06 14:12:53 +0330

    try no. 4 on SR fix in production

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('shown');
-        } else {
-            entry.target.classList.remove('shown')
-        }
-    })
+ScrollReveal({
+    reset: true
 });
 
-
-const animated = document.querySelectorAll('.animated');
-animated.forEach((el) => observer.observe(el));
+ScrollReveal().reveal('.description',{origin: 'top', delay: 50, distance: '20px'});
+ScrollReveal().reveal('.lesgo',{origin: 'bottom', delay: 50, distance: '15px'});
+ScrollReveal().reveal('.landscape',{origin: 'right', delay: 50, distance: '20px'});
+ScrollReveal().reveal('.models',{origin: 'left', delay: 50, distance: '20px'});
+ScrollReveal().reveal('.aboutmain',{origin: 'bottom', delay: 100, distance: '20px'});
+ScrollReveal().reveal('.about-title',{origin: 'bottom', delay: 100, distance: '20px'});
+ScrollReveal().reveal('.about-desc',{origin: 'bottom', delay: 200, distance: '20px'});
+ScrollReveal().reveal('.about-img',{origin: 'bottom', delay: 300, distance: '20px'});
+ScrollReveal().reveal('.card1',{origin: 'top', delay: 150, distance: '15px'});
+ScrollReveal().reveal('.card2',{origin: 'top', delay: 100, distance: '15px'});
+ScrollReveal().reveal('.card3',{origin: 'top', delay: 50, distance: '15px'});
+ScrollReveal().reveal('.gridsec',{origin: 'bottom', delay: 150, distance: '15px'});
+ScrollReveal().reveal('.mcmodel',{origin: 'bottom', delay: 150, distance: '20px'});
+ScrollReveal().reveal('.dlbutton',{origin: 'bottom', scale: 0.85, delay: 150, distance: '20px'});
+ScrollReveal().reveal('.footer',{origin: 'bottom', delay: 100, distance: '20px'});
 
 function copy_text() {
     const copyText = document.querySelector('#ip');  
